34dac6501a7de6d330764a802dc8756e65799359
Anis <chaabani.anis@gmail.com>
2019-07-30 22:51:41 +0200

    Initial commit

---
+++ InstrumentProcessorKata.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InstrumentProcessorKata.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
